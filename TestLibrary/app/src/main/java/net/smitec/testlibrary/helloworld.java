e3cab0a58ce5b777212224bf2b981881ed467165
Elliot Smith <elliot.smith91@gmail.com>
2014-09-24 14:34:29 +1000

    added both porjects

---
+++ TestLibrary/app/src/main/java/net/smitec/testlibrary/helloworld.java
@@ -0,0 +1,10 @@
+package net.smitec.testlibrary;
+
+/**
+ * Created by elliot on 24/09/2014.
+ */
+public class helloworld {
+    public static String talk() {
+        return "Hello World";
+    }
+}
